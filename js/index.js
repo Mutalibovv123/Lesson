1c7dd24b5ed4de4fe84b9144fd9b3043ca3bebd6
аппле <apple@MacBook-Air-apple.local>
2024-10-08 07:10:52 +0500

    LEsson

---
+++ js/index.js
@@ -0,0 +1,230 @@
+// 1-masala
+// let randomNumber = Math.trunc(Math.random()*100);
+// console.log(randomNumber);
+
+
+// 2-masala
+
+
+// let son = +prompt("Sonni kirting") ;
+// if (son < 0) {
+//     console.log("Musbat son kiriting");
+// } else {
+//     ildiz = Math.sqrt(son) ;
+//     console.log(ildiz);
+// }
+
+
+//3-masala
+// let num1 = +prompt ("1-sooni kiriting");
+// let num2 = +prompt ("1-sooni kiriting");
+// let num3 = +prompt ("1-sooni kiriting");
+// let num4 = +prompt ("1-sooni kiriting");
+// let num5 = +prompt ("1-sooni kiriting");
+
+
+// let max = Math.max (num1, num2, num3, num4, num5) ;
+// let min = Math.min (num1, num2, num3, num4, num5) ;
+// console.log(max)
+// console.log(min)
+
+
+// 4-masala
+// let son =+prompt("Son kirting") ;
+// let up = Math.floor(son);
+// let down = Math.ceil(son);
+// console.log(up);
+// console.log(down);
+
+
+// 5-masala
+
+
+// let r = +prompt("Aylananing radiusini kiriting");
+// let pi = 3.1415;
+// let length = 2 * pi * r;
+// console.log(length);
+
+
+// TERNARY
+// 1-misol
+// let num = +prompt("Sonni kiriting");
+// num > 0 ? console.log("Musbat") : console.log("Manfiy")
+
+
+// 2-misol
+
+
+// let age = ("Yoshingizni kiriting");
+// age >= 18 ? console.log("Ruxsat berilgan") : console.log("Ruxsat berilmagan");
+
+
+// 3-misol
+// let number = ("Sonni kiriting");
+// number % 2 == 0 ? console.log("Juft son") : console.log("Toq son");
+
+
+// For sikl operatorlar
+// 1-masala
+// for (let i = 1 ; i < 20 ; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// 2-masala
+// let sum = 0 ;
+// for (let i = 1; i<=10 ; i++) {
+//     let son = prompt(i + "Sonni kiriting")
+// }
+// if (son > 0) {
+//     sum += son;
+//     console.log(sum);
+// }
+// console.log(son);
+
+
+// 3-masala
+
+
+// for (let i =1 ; i <= 100 ; i++) {
+//     if (i >= 30 && i<=50) {
+//         console.log(i);
+//     }
+// }
+
+
+// 4-masala
+// let musbat = 0;
+// let manfiy = 0;
+// let son;
+// for (let i = 1; i <= 15; i++) {
+//     let son = prompt(i + "chi" + "Sonni kiriting")
+//     if (son > 0) {
+//         musbat++
+//     }
+//     else {
+//         manfiy++
+//     }
+// }
+// console.log(musbat)
+// console.log(manfiy)
+
+
+// 5- misol arrayda ekan, array hali o’tilgani yo’q
+
+
+// 6-masala
+// sum = 0 ;
+// for (let i = 1 ; i <=50 ; i++) {
+//     if (i % 2 == 1 ) {
+//         sum =+ i
+//     }
+// }
+// console.log(sum);
+
+
+// 7-masala
+
+
+// let son;
+// for (let i = 1; i <= 5; i++) {
+//     let son = prompt(i + "chi" + "sonni kiriting")
+//     if (son >= 18) {
+//         console.log("Kirishga ruxsat");
+//     } else {
+//         console.log("Kirishga ruxsat yo'q");
+//     }
+// }
+
+
+// 8-masala
+
+
+// let counter = 0;
+
+
+// for (let i = 1; i<=100; i++) {
+//     if (i % 2 == 0) {
+//         counter++
+//     }
+// }
+// console.log(counter);
+
+
+// 9-masala
+// let counter = 0;
+// let son;
+// for (let i = 1 ; i <= 10 ; i++ ) {
+//    let son = prompt(i + "chi" + "sonni kiriting");
+//    if (son >= 5 && son<=10) {
+//     counter ++ ;
+//    }
+// }
+// console.log(counter)
+
+
+// 10-masala
+
+
+// let son ;
+
+
+// for (let i =1 ; i<=10 ; i++) {
+//     let son = prompt(i + "ni" + "kiriting");
+//     if (son > 0) {
+//         console.log(son);
+//     }
+// }
+
+
+// 11-masala
+
+
+// let son;
+// let counter = 0;
+// for (let i =1 ; i <= 15 ; i++ ) {
+//     let son = prompt(i + "chi" + "Sonni kiriting") ;
+//     if (son % 2 == 0 || son < 0) {
+//         counter++
+//     }
+// }
+// console.log(counter);
+
+
+// 12-masala
+
+
+// for (let i = 1; i<=50 ; i++) {
+//     if (i % 3 == 0) {
+//       console.log(i);
+//     }
+// }
+
+
+// 13 - yoqku Google docsda
+
+
+// 14-masala
+
+
+// let son;
+
+
+// for (let i = 1; i<=10 ; i++) {
+//     let son = prompt(i + "ni" + "kiriting") ;
+//     if (son <= 50) {
+//         console.log(son);
+//     }
+// }
+
+
+// 15-masala
+// let son;
+// for (let i = 1; i <= 15 ; i++) {
+//     let son = prompt(i + "ni" + "kiriting") ;
+//     if (son == 0) {
+//         console.log("0 mavjud");
+//     }
+// }
